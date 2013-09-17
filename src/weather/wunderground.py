a2705791dd0f5fd753df40b1b784a9c39c337d0b
eugene wu <sirrice@gmail.com>
2013-09-16 20:05:49 -0400

    updated dataset descriptions and linked some of them

--- src/weather/wunderground.py
+++ src/weather/wunderground.py
@@ -1,21 +1,24 @@
 import requests
 import bsddb3
 import time
+import json
 from pyquery import PyQuery as pq
 
 URL = "http://www.wunderground.com/history/airport/KBOS/2012/%d/%d/DailyHistory.html?req_city=Cambridge&req_state=MA&req_statename=Massachusetts"
-FNAME = "./wunderground.bdb"
-db = bsddb3.hashopen(FNAME)
-
+URL = "http://api.wunderground.com/api/57929e89bff6b6d4/history_%s/q/MA/Boston.json"
+FNAME = "./wunderground.json"
+db = []
 
 months = [6,7]
 days = range(1,32)
 
 for month in months:
   for day in days:
-    url = URL % (month, day)
+    date = "2012%02d%02d" % (month, day)
+    url = URL % date
     html = requests.get(url).content
-    db["%d/%d" % (month, day)] = html
-    time.sleep(10)
+    data = json.loads(html)
+    db.append(data)
 
-db.close()
+with file(FNAME, 'w') as f:
+  f.write(json.dumps(db))
