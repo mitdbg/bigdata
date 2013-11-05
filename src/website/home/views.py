1d16a4d27c22b180021724750bbc24eb4fe27814
Ubuntu <ubuntu@csail.mit.edu>
2013-11-05 22:03:35 +0000

    syntax err

--- src/website/home/views.py
+++ src/website/home/views.py
@@ -53,7 +53,7 @@ def download(request, fname=None):
     '2012_08.json.gz',
     '2012_09.json.gz',
     '2012_10.json.gz',
-    '2012_11.json.gz'
+    '2012_11.json.gz',
     '2012_12.json.gz'
   ]
   valid_files += ['test.txt']
