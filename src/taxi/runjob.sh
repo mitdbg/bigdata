28994412ae82bfed6d0e77445bc05b923d09b82a
eugene wu <sirrice@gmail.com>
2013-12-02 12:40:40 -0500

    added clarification to prediction challenge

--- src/taxi/runjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 2 instances
-# j-3RBP0QV3PJXKH
-
-
-python mrfilter.py -r emr --emr-job-flow-id=j-3RBP0QV3PJXKH  -o "s3://mitbigdata/taxi/filtered" --no-output "s3://mitbigdata/taxi/sample.csv"
