60cea5a260e493c7d1b1a579b0d66369f4b79e2f
eugene wu <sirrice@gmail.com>
2013-12-02 13:38:17 -0500

    readded recompute scores

--- src/website/scripts/recompute_scores.py
+++ src/website/scripts/recompute_scores.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.path.abspath('.'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
