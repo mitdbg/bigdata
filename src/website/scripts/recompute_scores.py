80872da6468a50b3356ce3764f0ab8e6a2952a19
eugene wu <sirrice@gmail.com>
2013-12-02 15:14:18 -0500

    update send script

--- src/website/scripts/recompute_scores.py
+++ src/website/scripts/recompute_scores.py
@@ -1,12 +1,4 @@
-import os
-import sys
-sys.path.append(os.path.abspath('.'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-from home.models import *
-from home.util import *
+from header import *
 
 update = False
 if len(sys.argv) > 1:
