bfa0e49efe91de7c121ea463daee0bcfd74f3917
eugene wu <sirrice@gmail.com>
2013-12-02 13:39:33 -0500

    update recompute script

--- src/website/scripts/recompute_scores.py
+++ src/website/scripts/recompute_scores.py
@@ -8,7 +8,11 @@ application = get_wsgi_application()
 from home.models import *
 from home.util import *
 
-
+scores = []
 for s in  Submission.objects.all():
   score = compute_score(parse(s.text))
-  print s.user, score
+  scores.append((s.user, score))
+
+scores.sort(key=lambda p: p[1], reverse=True)
+for u,score in scores:
+  print u, score
