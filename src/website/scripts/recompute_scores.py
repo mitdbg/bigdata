f0ceafb75caeb2836d3c76ab282d9a236b43c93a
eugene wu <sirrice@gmail.com>
2013-12-02 13:41:14 -0500

    update recompute script

--- src/website/scripts/recompute_scores.py
+++ src/website/scripts/recompute_scores.py
@@ -8,9 +8,17 @@ application = get_wsgi_application()
 from home.models import *
 from home.util import *
 
+update = False
+if len(sys.argv) > 1:
+  if sys.argv[1] == 'update':
+    raw_input("updating are you sure?  ctrl-c to abort!")
+    update = True
+
 scores = []
 for s in  Submission.objects.all():
   score = compute_score(parse(s.text))
+  s.score = score
+  s.save()
   scores.append((s.user, score))
 
 scores.sort(key=lambda p: p[1], reverse=True)
